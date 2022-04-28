e3e662ccd1ddc0e3ade7206eba684e6c72746f58
mvstic <86735882+mvstic@users.noreply.github.com>
2022-04-28 10:29:27 -0500

    Add clearDisplay()

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const buttons = document.querySelectorAll("button");
 const operators = document.querySelectorAll(".operator");
 const display = document.querySelector("#display");
-const displayHistory = document.querySelector("#history");
+const history = document.querySelector("#history");
 const operatorBtns = document.querySelectorAll(".operator");
 const numericalBtns = document.querySelectorAll(".numeral");
+const decimalBtn = document.querySelector(".decimal");
 const equalBtn = document.querySelector(".equals");
 
 const btns = document.querySelectorAll(".calculator-buttons");
@@ -11,7 +12,6 @@ const clearBtn = document.querySelector("#clear");
 
 let array = [];
 
-let round = 1;
 let result = 0;
 
 let input;
@@ -35,10 +35,13 @@ function onOperatorPress(input) {
     input = display.value;
     if (array.length === 0) {
         intoArray(input)
+        clearDisplay();
     } else if (array.length === 1 && input.match(regex)) {
         intoArray(input);
+        clearDisplay();
     } else {
         intoArrayAndSolve(input);
+        clearDisplay();
     }
 }
 
@@ -56,6 +59,10 @@ function solve(array) {
 
 function clearArray() {
     return array = [];
+}
+
+function clearDisplay() {
+    return display.value = '';
 }
 
 function add(a, b) {
@@ -91,14 +98,21 @@ numericalBtns.forEach(button => {
         console.log(e.target.value);
         display.value += e.target.value;
     })
-})
+});
 
 operatorBtns.forEach(button => {
     button.addEventListener('click', (e) => {
         console.log(e.target.value);
         display.value += e.target.value;
     })
-})
+});
+
+decimalBtn.addEventListener('click', (e) => {
+    console.log(e.target.value);
+    display.value += e.target.value;
+});
+
+equalBtn.addEventListener('click', onOperatorPress)
 
 
 
